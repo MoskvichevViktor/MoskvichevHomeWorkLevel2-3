6743972dd134d724269acfa20bd4dc42b4b70cda
MoskvichevViktor <beldetal@yandex.ru>
2021-03-24 01:53:38 +0300

    finish

--- src/main/java/leveltwonumberthree/Main.java
+++ src/main/java/leveltwonumberthree/Main.java
@@ -1,18 +1,46 @@
 package leveltwonumberthree;
-/*Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов,
+/*1)Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов,
 из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.
 
-Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
+2)Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
 В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать
  номер телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов
 (в случае однофамильцев), тогда при запросе такой фамилии должны выводиться все телефоны. Желательно
 не добавлять лишний функционал (дополнительные поля (имя, отчество, адрес), взаимодействие с пользователем
  через консоль и т.д). Консоль использовать только для вывода результатов проверки телефонного справочника.*/
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args){
+        List<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("two");
+        words.add("one");
+        words.add("three");
+        words.add("one");
+        words.add("one");
+        words.add("two");
+        words.add("five");
+        words.add("one");
+        words.add("one");
+        words.add("three");
+        words.add("five");
+        words.add("one");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        Integer count;
+        for(String i : words){
+            count = map.get(i);
+            map.put( i, count == null ? 1 : count + 1);
+        }
+        System.out.println(map);
+
 
         Contakt contakt = new Contakt();
 
